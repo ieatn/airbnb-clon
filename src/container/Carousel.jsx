47f80ab6a76b57676906185ae4cdfd33e724b7e6
ieatn <81874063+ieatn@users.noreply.github.com>
2022-12-24 13:53:22 -0800

    starting carousel, cards

---
+++ src/container/Carousel.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Card from './Card'
+import './carousel.css'
+
+export default function Carousel() {
+  return (
+    <div className='carousel'>
+        <Card title='unique stays'/>
+        <Card title='unique stays'/>
+        <Card title='unique stays'/>
+        <Card title='unique stays'/>
+        <Card title='unique stays'/>
+        <Card title='unique stays'/>
+    </div>
+  )
+}
--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import Banner from '../components/Banner'
 import './Home.css'
+import Banner from '../components/Banner'
+import Carousel from './Carousel'
 
 const Home = () => (
   <div className='home'>
     <button>Search dates</button>
     <Banner />
+    <Carousel />
   </div>
 )
 export default Home
