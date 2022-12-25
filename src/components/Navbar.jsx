de12d3261de5ecd66d8b0e30da13aed4edde9b82
ieatn <ieatnubcake@gmail.com>
2022-12-25 13:09:02 -0800

    added material ui and  icons to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,25 @@
 import React from 'react'
 import './Navbar.css'
+import LanguageIcon from '@mui/icons-material/Language';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import SearchIcon from '@mui/icons-material/Search';
 
 export default function Navbar() {
   return (
     <nav>
       <div className="nav-left">
-        <img className='logo' style={{height: '50px'}} src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/2560px-Airbnb_Logo_B%C3%A9lo.svg.png" alt="" />  
+        <a href="#"><img className='logo' style={{height: '50px'}} src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/2560px-Airbnb_Logo_B%C3%A9lo.svg.png" alt="" /></a>  
       </div>
       <div className="nav-middle">
           <input className='searchBar' type="text" />
+          <SearchIcon/>
       </div>
       <div className="nav-right">
           <div>Become a Host</div>
-          <div>globe icon</div>
-          <div>down arrow</div>
-          <div>person icon</div>
+          <div><LanguageIcon fontSize='large' /></div>
+          <div><KeyboardArrowDownIcon fontSize='large'/></div>
+          <div><AccountCircleIcon fontSize='large'/></div>
       </div>
     </nav>
   )
