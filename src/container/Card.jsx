2b7d4838ebbcd1856c6f0f1697895d39fb36ea22
ieatn <ieatnubcake@gmail.com>
2022-12-25 13:48:19 -0800

    home and carousel finished

--- src/container/Card.jsx
+++ src/container/Card.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './Card.css'
 
-export default function Card({title, body}) {
+export default function Card({img, title, body, price}) {
   return (
     <div className='card'>
-      <img src="https://cdn.icon-icons.com/icons2/2620/PNG/512/among_us_player_red_icon_156942.png" alt="" />
+      <img src={img} alt="" />
       <div className="card-body">
         <h2>{title}</h2>
         <p>{body}</p>
+        <p>{price}</p>
       </div>
     </div>
   )
--- src/container/Carousel.jsx
+++ src/container/Carousel.jsx
@@ -5,12 +5,12 @@ import './carousel.css'
 export default function Carousel() {
   return (
     <div className='carousel'>
-        <Card title='unique stays' body='Unique activites we can do together, led by a world of hosts'/>
-        <Card title='unique stays' body='Unique activites we can do together, led by a world of hosts'/>
-        <Card title='unique stays' body='Unique activites we can do together, led by a world of hosts'/>
-        <Card title='unique stays' body='Unique activites we can do together, led by a world of hosts'/>
-        <Card title='unique stays' body='Unique activites we can do together, led by a world of hosts'/>
-        <Card title='unique stays' body='Unique activites we can do together, led by a world of hosts'/>
+      <Card price='' img='https://a0.muscache.com/im/pictures/eb9c7c6a-ee33-414a-b1ba-14e8860d59b3.jpg?im_w=720' title='unique stays' body='Unique activites we can do together, led by a world of hosts'/>
+      <Card price='' img='https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im_w=720' title='unique stays' body='Unique activites we can do together, led by a world of hosts'/>
+      <Card price='' img='https://a0.muscache.com/im/pictures/fdb46962-10c1-45fc-a228-d0b055411448.jpg?im_w=720' title='unique stays' body='Unique activites we can do together, led by a world of hosts'/>
+      <Card price='$350/night' img='https://media.nomadicmatt.com/2019/airbnb_breakup3.jpg' title='unique stays' body='Unique activites we can do together, led by a world of hosts'/>
+      <Card price='$350/night' img='https://thespaces.com/wp-content/uploads/2017/08/Courtesy-of-Airbnb.jpg' title='unique stays' body='Unique activites we can do together, led by a world of hosts'/>
+      <Card price='$350/night' img='https://media.nomadicmatt.com/2018/apartment.jpg' title='unique stays' body='Unique activites we can do together, led by a world of hosts'/>
     </div>
   )
 }
