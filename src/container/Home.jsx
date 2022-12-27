ba76b989ed9012803bf530b6760dffaf26bc07ba
ieatn <ieatnubcake@gmail.com>
2022-12-26 19:52:59 -0800

    working on date picker

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -3,13 +3,17 @@ import './Home.css'
 import Banner from '../components/Banner'
 import Carousel from './Carousel'
 import Button from '@mui/material/Button';
+import DatePicker from '@mui/material/DatePicker';
 
 export default function Home() {
   const [date, setDate] = useState(false)
 
   return (
     <div className='home'>
-      <Button onClick={() => setDate(!date)} variant="outlined">{date ? 'Hide': 'Search Dates'}</Button>
+      <Button onClick={() => setDate(!date)} variant="outlined">
+        {date ? 'Hide': 'Search Dates'}
+      </Button>
+      {date && <DatePicker />}
       <Banner />
       <Carousel />
   </div>
